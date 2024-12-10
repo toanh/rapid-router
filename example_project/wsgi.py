850e0edd9a07b6cb2c3f05c583a831766220a8c8
Toan Huynh <toan.kien@gmail.com>
2024-12-11 08:27:16 +1100

    tinkering with wsgi

--- example_project/wsgi.py
+++ example_project/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.settings")
 
 from django.core.wsgi import get_wsgi_application
 
